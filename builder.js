dd6330753068a73e9a3e0d40a379c7305020ca2c
igor-ruivo <igor.ruivo.97@gmail.com>
2021-02-16 20:48:14 +0000

    Format code

--- builder.js
+++ builder.js
@@ -18,7 +18,7 @@ const buildStorageWhenNeeded = true;
 const buildFarmWhenNeeded = true;
 const maxStoragePercentage = 98;
 const maxFarmPercentage = 98;
-const maxStorageLevel = 6;
+const maxStorageLevel = 30;
 const maxFarmLevel = 30;
 const maxBuildQueueLength = 2;
 const buildQueueOffset = 2;
@@ -27,17 +27,17 @@ const levels = loadBuildingsAndLevels();
 const userBuildList = loadUserBuildList();
 let clickedTask = {};
 
-(function() {
-    'use strict';
-	
+(function () {
+	'use strict';
+
 	const delay = Math.floor(Math.random() * 4000 + 43000);
-	
-	setTimeout(function(){
+
+	setTimeout(function () {
 		nextIteration();
 		console.log("Reload dentro de " + Math.round(delay / 1000) + " segundos...");
 	}, 3000);
-	
-	setTimeout(function(){
+
+	setTimeout(function () {
 		location.reload();
 	}, delay);
 })();
@@ -50,13 +50,13 @@ function getDate() {
 }
 
 function nextIteration() {
-    const nextBuildTask = getNextBuildTask();
-    if (nextBuildTask){
-        nextBuildTask.click();
-        console.log(getDate() + " - A upar " + clickedTask.building + " para nível " + clickedTask.level + ".");
-    }
+	const nextBuildTask = getNextBuildTask();
+	if (nextBuildTask) {
+		nextBuildTask.click();
+		console.log(getDate() + " - A upar " + clickedTask.building + " para nível " + clickedTask.level + ".");
+	}
 	else {
-        console.log(getDate() + " - De momento não é possível construir nada.");
+		console.log(getDate() + " - De momento não é possível construir nada.");
 	}
 }
 
@@ -65,7 +65,7 @@ function buildingAlreadyInQueue(build) {
 }
 
 function checkStorageForUpgrade(currentLevel) {
-	if(currentLevel < maxStorageLevel) {
+	if (currentLevel < maxStorageLevel) {
 		const storageSpace = document.getElementById("storage").innerText;
 		const wood = document.getElementById("wood").innerText;
 		const stone = document.getElementById("stone").innerText;
@@ -73,21 +73,21 @@ function checkStorageForUpgrade(currentLevel) {
 		const maxResource = Math.max(Math.max(wood, stone), iron);
 		const currentStoragePercentage = Math.round(maxResource / storageSpace * 100);
 		console.log("Armazém a " + currentStoragePercentage + "% (Nível " + currentLevel + ")");
-		if(currentStoragePercentage >= maxStoragePercentage && !buildingAlreadyInQueue("storage")) {
-			userBuildList.unshift({building: "storage", level: 30});
+		if (currentStoragePercentage >= maxStoragePercentage && !buildingAlreadyInQueue("storage")) {
+			userBuildList.unshift({ building: "storage", level: 30 });
 			console.log("Armazém adicionado à lista...");
 		}
 	}
 }
 
 function checkFarmForUpgrade(currentLevel) {
-	if(currentLevel < maxFarmLevel) {
+	if (currentLevel < maxFarmLevel) {
 		const maxPopulation = document.getElementById("pop_max_label").innerText;
 		const currentPopulation = document.getElementById("pop_current_label").innerText;
 		const currentFarmPercentage = Math.round(currentPopulation / maxPopulation * 100);
 		console.log("Fazenda a " + currentFarmPercentage + "% (Nível " + currentLevel + ")");
-		if(currentFarmPercentage >= maxFarmPercentage && !buildingAlreadyInQueue("farm")) {
-			userBuildList.push({building: "farm", level: 30});
+		if (currentFarmPercentage >= maxFarmPercentage && !buildingAlreadyInQueue("farm")) {
+			userBuildList.push({ building: "farm", level: 30 });
 			console.log("Fazenda adicionada à lista...");
 		}
 	}
@@ -95,9 +95,9 @@ function checkFarmForUpgrade(currentLevel) {
 
 function removeCompletedTasks(list) {
 	const newBuildList = [];
-	for(let i = 0; i < list.length; i++) {
+	for (let i = 0; i < list.length; i++) {
 		const currentBuilding = list[i];
-		if(currentBuilding.level >= levels[currentBuilding.building]) {
+		if (currentBuilding.level >= levels[currentBuilding.building]) {
 			newBuildList.push(currentBuilding);
 		}
 	}
@@ -105,35 +105,35 @@ function removeCompletedTasks(list) {
 }
 
 function getNextBuildTask() {
-	if(buildStorageWhenNeeded) {
+	if (buildStorageWhenNeeded) {
 		checkStorageForUpgrade(levels["storage"] - 1);
 	}
-	if(buildFarmWhenNeeded) {
+	if (buildFarmWhenNeeded) {
 		checkFarmForUpgrade(levels["farm"] - 1);
 	}
-	if($('[id="buildqueue"]').find('tr').length >= buildQueueOffset + maxBuildQueueLength) {
+	if ($('[id="buildqueue"]').find('tr').length >= buildQueueOffset + maxBuildQueueLength) {
 		console.log("Já há " + maxBuildQueueLength + " ordens ou mais na fila de construição. Não vou fazer nada agora.");
 		return undefined;
-    }
-    console.log(levels);
-    for(let i = 0; i < userBuildList.length; i++){
+	}
+	console.log(levels);
+	for (let i = 0; i < userBuildList.length; i++) {
 		const building = userBuildList[i].building;
 		const level = levels[building];
-        console.log("A tentar upar " + building + " para nível " + level + "...");
-        const nextLink = buildLinkName(building, level);
+		console.log("A tentar upar " + building + " para nível " + level + "...");
+		const nextLink = buildLinkName(building, level);
 		const linkElement = document.getElementById(nextLink);
-        if (linkElement) {
-            const isClickable = linkElement.offsetWidth > 0 || linkElement.offsetHeight > 0;
-            if (isClickable) {
-				clickedTask = {building: building, level: level};
-                return linkElement;
-            }
-            if (forceOrder) {
-                return undefined;
-            }
-        }
-    }
-    return undefined;
+		if (linkElement) {
+			const isClickable = linkElement.offsetWidth > 0 || linkElement.offsetHeight > 0;
+			if (isClickable) {
+				clickedTask = { building: building, level: level };
+				return linkElement;
+			}
+			if (forceOrder) {
+				return undefined;
+			}
+		}
+	}
+	return undefined;
 }
 
 function buildLinkName(building, level) {
@@ -150,16 +150,12 @@ function loadBuildingsAndLevels() {
 }
 
 function loadUserBuildList() {
-    const buildList = [];
+	const buildList = [];
 
-//change
-    buildList.push({building: "wood", level: 15});
-    buildList.push({building: "stone", level: 15});
-    buildList.push({building: "main", level: 10});
-    buildList.push({building: "barracks", level: 5});
-    buildList.push({building: "smith", level: 5});
-    buildList.push({building: "stable", level: 1});
-    buildList.push({building: "iron", level: 10});
-	
-    return removeCompletedTasks(buildList);
+	//change
+	buildList.push({ building: "wood", level: 30 });
+	buildList.push({ building: "stone", level: 30 });
+	buildList.push({ building: "iron", level: 30 });
+
+	return removeCompletedTasks(buildList);
 }
